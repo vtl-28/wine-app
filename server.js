89eb4cd7a882592e7483057b83cbdc86d1f252dc
Vuyisile <vtlehola23@gmail.com>
2024-01-12 05:38:28 +0200

    fix route link

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
-const book_router = require('./routes/book.router')
+const book_router = require('./routes/book_route')
 
 const PORT = process.env.PORT || 3001;
 
