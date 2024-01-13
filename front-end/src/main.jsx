3d6016b8ae38cc76a48db0c139206001a6c199c9
Vuyisile <vtlehola23@gmail.com>
2024-01-13 21:00:28 +0200

    setup react-router

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+import { ChakraProvider } from '@chakra-ui/react'
 import App from './App.jsx'
+import ErrorPage from './error_page.jsx'
+import LoginPage from './Pages/Login/index.jsx'
+import LandingPage from './Pages/Landing Page/index.jsx'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginPage />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <ChakraProvider>
+        <RouterProvider router={router} />
+      </ChakraProvider>
   </React.StrictMode>,
 )
